8a6c4a4abbd7e104464afaf098f46dd27e68c719
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 14:12:49 -0500

    Update and rename animals-part-4.js to desertAnimals.js

---
+++ desertAnimals.js
@@ -0,0 +1,8 @@
+export const desertAnimals = [
+  { name: "Meerkat", color: "sandybrown", biome: "desert", evolveScore: 0, icon: "images/meerkat.png" },
+  { name: "Lizard", color: "green", biome: "desert", evolveScore: 5, icon: "images/lizard.png" },
+  { name: "Fennec Fox", color: "wheat", biome: "desert", evolveScore: 15, icon: "images/fennec_fox.png" },
+  { name: "Hyena", color: "#8B4513", biome: "desert", evolveScore: 30, icon: "images/hyena.png" },
+  { name: "Camel", color: "#DEB887", biome: "desert", evolveScore: 50, icon: "images/camel.png" },
+  { name: "Vulture", color: "brown", biome: "desert", evolveScore: 100, icon: "images/vulture.png" }
+];
