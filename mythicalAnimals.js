c7fbca31dfca0f77f407150e2394942bb580fad5
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 14:13:13 -0500

    Update and rename animals-part-5.js to mythicalAnimals.js

---
+++ mythicalAnimals.js
@@ -0,0 +1,5 @@
+export const mythicalAnimals = [
+  { name: "Phoenix", color: "gold", biome: "mythical", evolveScore: 350, icon: "images/phoenix.png" },
+  { name: "Hydra", color: "darkgreen", biome: "mythical", evolveScore: 400, icon: "images/hydra.png" },
+  { name: "Griffin", color: "goldenrod", biome: "mythical", evolveScore: 450, icon: "images/griffin.png" }
+];
