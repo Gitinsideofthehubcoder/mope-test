2a8951b2490979adabe2ed3115178a3c0ea047a7
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 14:10:36 -0500

    Update and rename animals-part-1.js to landAnimals.js

---
+++ landAnimals.js
@@ -0,0 +1,14 @@
+export const landAnimals = [
+  { name: "Mouse", color: "gray", biome: "land", evolveScore: 0, icon: "images/mouse.png" },
+  { name: "Rabbit", color: "pink", biome: "land", evolveScore: 5, icon: "images/rabbit.png" },
+  { name: "Pig", color: "brown", biome: "land", evolveScore: 15, icon: "images/pig.png" },
+  { name: "Deer", color: "tan", biome: "land", evolveScore: 30, icon: "images/deer.png" },
+  { name: "Fox", color: "orange", biome: "land", evolveScore: 50, icon: "images/fox.png" },
+  { name: "Wolf", color: "lightgray", biome: "land", evolveScore: 70, icon: "images/wolf.png" },
+  { name: "Lion", color: "goldenrod", biome: "land", evolveScore: 100, icon: "images/lion.png" },
+  { name: "Bear", color: "#654321", biome: "land", evolveScore: 130, icon: "images/bear.png" },
+  { name: "Rhino", color: "gray", biome: "land", evolveScore: 200, icon: "images/rhino.png" },
+  { name: "Elephant", color: "lightgray", biome: "land", evolveScore: 250, icon: "images/elephant.png" },
+  { name: "Mammoth", color: "#70543E", biome: "land", evolveScore: 300, icon: "images/mammoth.png" },
+  { name: "Dragon", color: "red", biome: "land", evolveScore: 400, icon: "images/dragon.png" }
+];
