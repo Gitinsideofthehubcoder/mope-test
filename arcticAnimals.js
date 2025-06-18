d359f2bfbd0ecc57e56b6fad8fc97f2e57ae2261
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 14:11:35 -0500

    Update and rename animals-part-3.js to arcticAnimals.js

---
+++ arcticAnimals.js
@@ -0,0 +1,8 @@
+export const arcticAnimals = [
+  { name: "Arctic Hare", color: "white", biome: "arctic", evolveScore: 0, icon: "images/arctic_hare.png" },
+  { name: "Penguin", color: "black", biome: "arctic", evolveScore: 5, icon: "images/penguin.png" },
+  { name: "Seal", color: "gray", biome: "arctic", evolveScore: 15, icon: "images/seal.png" },
+  { name: "Walrus", color: "#A0522D", biome: "arctic", evolveScore: 50, icon: "images/walrus.png" },
+  { name: "Polar Bear", color: "white", biome: "arctic", evolveScore: 100, icon: "images/polar_bear.png" },
+  { name: "Yeti", color: "#E0FFFF", biome: "arctic", evolveScore: 150, icon: "images/yeti.png" }
+];
