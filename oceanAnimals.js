8170c8c6aa91ecc166b8abee0a48ce0be20ddc11
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 14:11:04 -0500

    Update and rename animals-part-2.js to oceanAnimals.js

---
+++ oceanAnimals.js
@@ -0,0 +1,9 @@
+export const oceanAnimals = [
+  { name: "Trout", color: "#00BFFF", biome: "ocean", evolveScore: 0, icon: "images/trout.png" },
+  { name: "Piranha", color: "red", biome: "ocean", evolveScore: 5, icon: "images/piranha.png" },
+  { name: "Tuna", color: "blue", biome: "ocean", evolveScore: 15, icon: "images/tuna.png" },
+  { name: "Shark", color: "gray", biome: "ocean", evolveScore: 50, icon: "images/shark.png" },
+  { name: "Orca", color: "black", biome: "ocean", evolveScore: 100, icon: "images/orca.png" },
+  { name: "Blue Whale", color: "#4682B4", biome: "ocean", evolveScore: 150, icon: "images/blue_whale.png" },
+  { name: "Kraken", color: "purple", biome: "ocean", evolveScore: 250, icon: "images/kraken.png" }
+];
