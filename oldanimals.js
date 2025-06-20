1a47c3f37784fa92dee394479597999fae80cbc0
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-20 13:20:59 -0500

    Update oldanimals.js

--- oldanimals.js
+++ oldanimals.js
@@ -1,1 +1,13 @@
+import { landAnimals } from './landAnimals.js';
+import { oceanAnimals } from './oceanAnimals.js';
+import { arcticAnimals } from './arcticAnimals.js';
+import { desertAnimals } from './desertAnimals.js';
+import { mythicalAnimals } from './mythicalAnimals.js';
 
+export const animals = [
+  ...landAnimals,
+  ...oceanAnimals,
+  ...arcticAnimals,
+  ...desertAnimals,
+  ...mythicalAnimals
+];
