5c07ef31dd50ddfc0afa98f831c56637fd0871ca
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-20 13:21:31 -0500

    Update animals.js

--- animals.js
+++ animals.js
@@ -1,13 +1,114 @@
-import { landAnimals } from './landAnimals.js';
-import { oceanAnimals } from './oceanAnimals.js';
-import { arcticAnimals } from './arcticAnimals.js';
-import { desertAnimals } from './desertAnimals.js';
-import { mythicalAnimals } from './mythicalAnimals.js';
-
 export const animals = [
-  ...landAnimals,
-  ...oceanAnimals,
-  ...arcticAnimals,
-  ...desertAnimals,
-  ...mythicalAnimals
+  // Tier 1
+  { name: "Mouse", biome: "Land", evolveScore: 0, icon: "icons/mouse.png" },
+  { name: "Shrimp", biome: "Ocean", evolveScore: 0, icon: "icons/shrimp.png" },
+  { name: "Chipmunk", biome: "Land", evolveScore: 0, icon: "icons/chipmunk.png" },
+  { name: "Desert Rat", biome: "Desert", evolveScore: 0, icon: "icons/desert_rat.png" },
+  { name: "Tundra Lemming", biome: "Tundra", evolveScore: 0, icon: "icons/tundra_lemming.png" },
+
+  // Tier 2
+  { name: "Rabbit", biome: "Land", evolveScore: 10, icon: "icons/rabbit.png" },
+  { name: "Trout", biome: "Ocean", evolveScore: 10, icon: "icons/trout.png" },
+  { name: "Arctic Hare", biome: "Tundra", evolveScore: 10, icon: "icons/arctic_hare.png" },
+  { name: "Desert Chipmunk", biome: "Desert", evolveScore: 10, icon: "icons/desert_chipmunk.png" },
+  { name: "Chub", biome: "Ocean", evolveScore: 10, icon: "icons/chub.png" },
+
+  // Tier 3
+  { name: "Mole", biome: "Land", evolveScore: 25, icon: "icons/mole.png" },
+  { name: "Crab", biome: "Ocean", evolveScore: 25, icon: "icons/crab.png" },
+  { name: "Penguin", biome: "Tundra", evolveScore: 25, icon: "icons/penguin.png" },
+  { name: "Meerkat", biome: "Desert", evolveScore: 25, icon: "icons/meerkat.png" },
+  { name: "Flying Fish", biome: "Ocean", evolveScore: 25, icon: "icons/flying_fish.png" },
+
+  // Tier 4
+  { name: "Pig", biome: "Land", evolveScore: 50, icon: "icons/pig.png" },
+  { name: "Salmon", biome: "Ocean", evolveScore: 50, icon: "icons/salmon.png" },
+  { name: "Seal", biome: "Tundra", evolveScore: 50, icon: "icons/seal.png" },
+  { name: "Armadillo", biome: "Desert", evolveScore: 50, icon: "icons/armadillo.png" },
+  { name: "Kakapo", biome: "Land", evolveScore: 50, icon: "icons/kakapo.png" },
+
+  // Tier 5
+  { name: "Deer", biome: "Land", evolveScore: 100, icon: "icons/deer.png" },
+  { name: "Squid", biome: "Ocean", evolveScore: 100, icon: "icons/squid.png" },
+  { name: "Reindeer", biome: "Tundra", evolveScore: 100, icon: "icons/reindeer.png" },
+  { name: "Gazelle", biome: "Desert", evolveScore: 100, icon: "icons/gazelle.png" },
+  { name: "Swordfish", biome: "Ocean", evolveScore: 100, icon: "icons/swordfish.png" },
+
+  // Tier 6
+  { name: "Fox", biome: "Land", evolveScore: 200, icon: "icons/fox.png" },
+  { name: "Hedgehog", biome: "Land", evolveScore: 200, icon: "icons/hedgehog.png" },
+  { name: "Jellyfish", biome: "Ocean", evolveScore: 200, icon: "icons/jellyfish.png" },
+  { name: "Arctic Fox", biome: "Tundra", evolveScore: 200, icon: "icons/arctic_fox.png" },
+  { name: "Swan", biome: "Land", evolveScore: 200, icon: "icons/swan.png" },
+
+  // Tier 7
+  { name: "Zebra", biome: "Land", evolveScore: 400, icon: "icons/zebra.png" },
+  { name: "Turkey", biome: "Land", evolveScore: 400, icon: "icons/turkey.png" },
+  { name: "Turtle", biome: "Ocean", evolveScore: 400, icon: "icons/turtle.png" },
+  { name: "Muskrat", biome: "Tundra", evolveScore: 400, icon: "icons/muskrat.png" },
+  { name: "Warthog", biome: "Desert", evolveScore: 400, icon: "icons/warthog.png" },
+
+  // Tier 8
+  { name: "Cobra", biome: "Desert", evolveScore: 800, icon: "icons/cobra.png" },
+  { name: "Stingray", biome: "Ocean", evolveScore: 800, icon: "icons/stingray.png" },
+  { name: "Mature Owl", biome: "Land", evolveScore: 800, icon: "icons/mature_owl.png" },
+  { name: "Camel", biome: "Desert", evolveScore: 800, icon: "icons/camel.png" },
+  { name: "Raven", biome: "Land", evolveScore: 800, icon: "icons/raven.png" },
+
+  // Tier 9
+  { name: "Cheetah", biome: "Land", evolveScore: 1500, icon: "icons/cheetah.png" },
+  { name: "Gourami", biome: "Ocean", evolveScore: 1500, icon: "icons/gourami.png" },
+  { name: "Pufferfish", biome: "Ocean", evolveScore: 1500, icon: "icons/pufferfish.png" },
+  { name: "Savannah Lizard", biome: "Desert", evolveScore: 1500, icon: "icons/savannah_lizard.png" },
+  { name: "Turkey", biome: "Land", evolveScore: 1500, icon: "icons/turkey.png" },
+
+  // Tier 10
+  { name: "Bear", biome: "Land", evolveScore: 2500, icon: "icons/bear.png" },
+  { name: "Swordfish", biome: "Ocean", evolveScore: 2500, icon: "icons/swordfish.png" },
+  { name: "Wolf", biome: "Land", evolveScore: 2500, icon: "icons/wolf.png" },
+  { name: "Gold Bear", biome: "Land", evolveScore: 2500, icon: "icons/gold_bear.png" },
+  { name: "Hyena", biome: "Desert", evolveScore: 2500, icon: "icons/hyena.png" },
+
+  // Tier 11
+  { name: "Crocodile", biome: "Land", evolveScore: 3500, icon: "icons/crocodile.png" },
+  { name: "Lion Seal", biome: "Ocean", evolveScore: 3500, icon: "icons/lion_seal.png" },
+  { name: "Octopus", biome: "Ocean", evolveScore: 3500, icon: "icons/octopus.png" },
+  { name: "Polar Bear", biome: "Tundra", evolveScore: 3500, icon: "icons/polar_bear.png" },
+  { name: "Vulture", biome: "Land", evolveScore: 3500, icon: "icons/vulture.png" },
+
+  // Tier 12
+  { name: "Rhino", biome: "Land", evolveScore: 5000, icon: "icons/rhino.png" },
+  { name: "Shark", biome: "Ocean", evolveScore: 5000, icon: "icons/shark.png" },
+  { name: "Wolverine", biome: "Tundra", evolveScore: 5000, icon: "icons/wolverine.png" },
+  { name: "Bison", biome: "Land", evolveScore: 5000, icon: "icons/bison.png" },
+
+  // Tier 13
+  { name: "Moose", biome: "Tundra", evolveScore: 6500, icon: "icons/moose.png" },
+  { name: "Killer Whale", biome: "Ocean", evolveScore: 6500, icon: "icons/killer_whale.png" },
+  { name: "Sabertooth", biome: "Tundra", evolveScore: 6500, icon: "icons/sabertooth.png" },
+  { name: "Komodo Dragon", biome: "Land", evolveScore: 6500, icon: "icons/komodo_dragon.png" },
+  { name: "White Moose", biome: "Tundra", evolveScore: 6500, icon: "icons/white_moose.png" },
+
+  // Tier 14
+  { name: "Elephant", biome: "Land", evolveScore: 8000, icon: "icons/elephant.png" },
+  { name: "Giant Squid", biome: "Ocean", evolveScore: 8000, icon: "icons/giant_squid.png" },
+  { name: "Blue Whale", biome: "Ocean", evolveScore: 8000, icon: "icons/blue_whale.png" },
+  { name: "Mammoth", biome: "Tundra", evolveScore: 8000, icon: "icons/mammoth.png" },
+  { name: "Black Widow", biome: "Land", evolveScore: 8000, icon: "icons/black_widow.png" },
+
+  // Tier 15
+  { name: "Dragon", biome: "Mythical", evolveScore: 10000, icon: "icons/dragon.png" },
+  { name: "Kraken", biome: "Mythical", evolveScore: 10000, icon: "icons/kraken.png" },
+  { name: "Yeti", biome: "Mythical", evolveScore: 10000, icon: "icons/yeti.png" },
+  { name: "Snowman", biome: "Mythical", evolveScore: 10000, icon: "icons/snowman.png" },
+
+  // Tier 16
+  { name: "Phoenix", biome: "Mythical", evolveScore: 12000, icon: "icons/phoenix.png" },
+  { name: "Bigfoot", biome: "Mythical", evolveScore: 12000, icon: "icons/bigfoot.png" },
+
+  // Tier 17
+  { name: "Dino Monster", biome: "Mythical", evolveScore: 15000, icon: "icons/dino_monster.png" },
+  { name: "Sea Monster", biome: "Mythical", evolveScore: 15000, icon: "icons/sea_monster.png" },
+  { name: "Black Dragon", biome: "Mythical", evolveScore: 20000, icon: "icons/black_dragon.png" },
+  { name: "Land Monster", biome: "Mythical", evolveScore: 20000, icon: "icons/land_monster.png" }
 ];
